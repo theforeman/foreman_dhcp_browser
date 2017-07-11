dbc9c3dcc642992a7695ae7f61dee2194cefa260
Ohad Levy <ohadlevy@gmail.com>
2017-07-11 18:54:26 +0300

    makes sure plugin loads correctly under rails 5.

--- lib/foreman_dhcp_browser/engine.rb
+++ lib/foreman_dhcp_browser/engine.rb
@@ -1,7 +1,7 @@
 require 'deface'
 module ForemanDhcpBrowser
   class Engine < ::Rails::Engine
-    initializer 'foreman_dhcp_browser.register_plugin', :after=> :finisher_hook do |app|
+    initializer 'foreman_dhcp_browser.register_plugin', :before=> :finisher_hook do |app|
       Foreman::Plugin.register :foreman_dhcp_browser do
       end
     end
