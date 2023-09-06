3a4f63fec8c7b2facb6335f0738f191dc9e86c1a
Bill Sirinek <bill@sirinek.com>
2023-09-06 11:01:03 -0500

    adding permissions to use the dhcp browser for non-administrators

--- lib/foreman_dhcp_browser/engine.rb
+++ lib/foreman_dhcp_browser/engine.rb
@@ -3,6 +3,21 @@ module ForemanDhcpBrowser
   class Engine < ::Rails::Engine
     initializer 'foreman_dhcp_browser.register_plugin', before: :finisher_hook do |_app|
       Foreman::Plugin.register :foreman_dhcp_browser do
+        security_block :foreman_dhcp_browser do
+          permission :view_dhcp, {
+            :dhcp => [:index, :show, :find_proxy, :find_record]
+          }
+          permission :new_dhcp, {
+            :dhcp => [:new, :create]
+          }
+          permission :edit_dhcp, {
+            :dhcp => [:edit, :update]
+          }
+          permission :delete_dhcp, {
+            :dhcp => [:destroy]
+          }
+        end
+        add_all_permissions_to_default_roles
       end
     end
 
