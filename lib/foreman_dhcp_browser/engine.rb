theforeman/foreman_dhcp_browser

commit 13ce09e0522eda83bbeca08f12b6ad54ea8d65c1

Compatibility with Zeitwerk

lib/foreman_dhcp_browser/engine.rb
@@ -1,8 +1,13 @@
 require 'deface'
-module ForemanDhcpBrowser
+module ForemanDHCPBrowser
   class Engine < ::Rails::Engine
-    initializer 'foreman_dhcp_browser.register_plugin', before: :finisher_hook do |_app|
-      Foreman::Plugin.register :foreman_dhcp_browser do
+    engine_name 'foreman_dhcp_browser'
+
+    initializer 'foreman_dhcp_browser.register_plugin', before: :finisher_hook do |app|
+      app.reloader.to_prepare do
+        Foreman::Plugin.register :foreman_dhcp_browser do
+          requires_foreman '>= 3.14.0'
+        end
       end
     end
 
@@ -10,7 +15,7 @@ module ForemanDhcpBrowser
       ::Net::Record.include ::ActiveModel::AttributeMethods
       ::Net::Record.include ::ActiveModel::Conversion
       ::Net::Record.extend ::ActiveModel::Naming
-      ::Net::Record.include ForemanDhcpBrowser::Concerns::NetRecordExtension
+      ::Net::Record.include ForemanDHCPBrowser::Concerns::NetRecordExtension
     end
   end
 end

Rakefile
@@ -14,7 +14,7 @@ end
 
 RDoc::Task.new(:rdoc) do |rdoc|
   rdoc.rdoc_dir = 'rdoc'
-  rdoc.title    = 'ForemanDhcpBrowser'
+  rdoc.title    = 'ForemanDHCPBrowser'
   rdoc.options << '--line-numbers'
   rdoc.rdoc_files.include('README.rdoc')
   rdoc.rdoc_files.include('lib/**/*.rb')

app/controllers/dhcp_controller.rb
@@ -1,4 +1,4 @@
-class DhcpController < ApplicationController
+class DHCPController < ApplicationController
   before_action :find_proxy
   before_action :find_record, only: %i[show edit update destroy]
 
@@ -45,7 +45,7 @@ class DhcpController < ApplicationController
     return not_found if params[:subnet_id].blank?
     @subnet = ::Subnet.find(params[:subnet_id])
     return not_found unless @subnet.try(:dhcp?)
-    @entries = DhcpEntries.new(subnet: @subnet)
+    @entries = DHCPEntries.new(subnet: @subnet)
   end
 
   def find_record

app/models/dhcp_entries.rb
@@ -1,4 +1,4 @@
-class DhcpEntries
+class DHCPEntries
   delegate :dhcp_proxy, to: :subnet
   alias_attribute :proxy, :dhcp_proxy
 

app/models/foreman_dhcp_browser/concerns/net_record_extension.rb
@@ -1,4 +1,4 @@
-module ForemanDhcpBrowser::Concerns::NetRecordExtension
+module ForemanDHCPBrowser::Concerns::NetRecordExtension
   extend ActiveSupport::Concern
 
   included do

foreman_dhcp_browser.gemspec
@@ -6,7 +6,7 @@ require 'foreman_dhcp_browser/version'
 # Describe your gem and declare its dependencies:
 Gem::Specification.new do |s|
   s.name        = 'foreman_dhcp_browser'
-  s.version     = ForemanDhcpBrowser::VERSION
+  s.version     = ForemanDHCPBrowser::VERSION
   s.license     = 'GPL-3.0'
   s.authors     = ['Ohad Levy']
   s.email       = 'ohadlevy@gmail.com'

lib/foreman_dhcp_browser.rb
@@ -1,4 +1,4 @@
 require 'foreman_dhcp_browser/engine'
 
-module ForemanDhcpBrowser
+module ForemanDHCPBrowser
 end

lib/foreman_dhcp_browser/version.rb
@@ -1,3 +1,3 @@
-module ForemanDhcpBrowser
+module ForemanDHCPBrowser
   VERSION = '0.0.8'.freeze
 end

commit 994234968cbfa9b12174d3c2045cea9d39ec04ac

Bring back compatibility with Foreman 3.13
Foreman started up with 3.13 so I think the Zeitwerk changes were
compatible. This is important, because in 3.13 it broke hard which
blocks users.

Fixes: 13ce09e0522e ("Compatibility with Zeitwerk")

lib/foreman_dhcp_browser/engine.rb
@@ -6,7 +6,7 @@ module ForemanDHCPBrowser
     initializer 'foreman_dhcp_browser.register_plugin', before: :finisher_hook do |app|
       app.reloader.to_prepare do
         Foreman::Plugin.register :foreman_dhcp_browser do
-          requires_foreman '>= 3.14.0'
+          requires_foreman '>= 3.13.0'
         end
       end
     end